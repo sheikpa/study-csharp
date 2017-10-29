eb2269fdbf5e584c5d5cc284c3ded4ab76e3c820
ayumusato <googays@grace.ocn.ne.jp>
2017-10-29 15:54:06 +0900

    ファイル追加

---
+++ basic/a.sato/Problem3_5/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("i=" + i);
+            if (i > 30 && i <= 70)
+            {
+                Console.WriteLine("30より大きく70以下です");
+            }
+        }
+    }
+}
