a4857344df44ac284b289d579939e08e91e90b60
ayumusato <googays@grace.ocn.ne.jp>
2017-11-14 22:34:36 +0900

    練習問題8-5修正

--- basic/a.sato/study-csharp-basic-4days-after/Problem8_5/Program.cs
+++ basic/a.sato/study-csharp-basic-4days-after/Problem8_5/Program.cs
@@ -12,21 +12,10 @@ namespace Problem8_5
         {
             for (int i = 1; i < 101; i++)
             {
-                if (i % 3 == 0)
+                if (i % 3 == 0 || i.ToString().Contains("3"))
                 {
                     Console.WriteLine(i);
                 }
-                else
-                {
-                    for (int j = 0; j < i.ToString().Length; j++)
-                    {
-                        if (i.ToString().Substring(j, 1) == "3")
-                        {
-                            Console.WriteLine(i);
-                        }
-                    }
-                }
-
             }
         }
     }
