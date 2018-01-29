765c072a43e27e6cefa61d48957230684daf0856
inabakousuke <34599882+inabakousuke@users.noreply.github.com>
2018-01-30 00:47:02 +0900

    練習問題2-5まで？

---
+++ basic/inaba/Problem2_4/Problem2_4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("キーボードから文字列を入力:");
+            string str = Console.ReadLine();
+            Console.WriteLine("キーボードから入力された文字列:" + str);
+        }
+    }
+}
