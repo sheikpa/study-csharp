d1f8de5e86b7096996b17d277e9c05229619eba6
inabakousuke <34599882+inabakousuke@users.noreply.github.com>
2018-01-30 00:57:19 +0900

    練習問題3-18まで

---
+++ basic/inaba/Problem3_17/Problem3_17/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_17
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください(1-100)：");
+            int i = int.Parse(Console.ReadLine());
+            if (i > 0 && i < 101)
+            {
+                if (i % 2 == 0)
+                {
+                    if (i <= 50)
+                    {
+                        Console.WriteLine("50以下の偶数です。");
+                    }
+                    else
+                    {
+                        Console.WriteLine("偶数です。");
+                    }
+                }
+                else if (i <= 50)
+                {
+                    Console.WriteLine("50以下です。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("適切な値を入力してください。");
+            }
+        }
+    }
+}
---
+++ basic/inaba/Problem3_18/Problem3_18/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("温度を入力してください(-10～35)：");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("摂氏" + i + "度");
+            Console.WriteLine("");
+            if (i > -11 && i < 36)
+            {
+                if (i >= 30)
+                {
+                    Console.WriteLine("真夏日です。");
+                }
+                else if (i >= 25 && i < 30)
+                {
+                    Console.WriteLine("夏日です。");
+                }
+                else if (i < 0)
+                {
+                    Console.WriteLine("真冬日です。");
+                }
+            }
+            else
+            {
+                Console.WriteLine("適切な値を入力してください。");
+            }
+        }
+    }
+}
