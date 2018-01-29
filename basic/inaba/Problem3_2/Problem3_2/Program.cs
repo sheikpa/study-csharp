274f5c3f66c2235b859d84dfa216ccd6d0376367
inabakousuke <34599882+inabakousuke@users.noreply.github.com>
2018-01-30 00:49:41 +0900

    練習問題3-5

---
+++ basic/inaba/Problem3_2/Problem3_2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("i=" + i);
+            if (i != 4)
+            {
+                Console.WriteLine("4ではありません");
+            }
+        }
+    }
+}
---
+++ basic/inaba/Problem3_3/Problem3_3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("整数を入力してください:");
+            int i = int.Parse(Console.ReadLine());
+            Console.WriteLine("i=" + i);
+            if (i < 50)
+            {
+                Console.WriteLine("50未満です");
+            }
+        }
+    }
+}
