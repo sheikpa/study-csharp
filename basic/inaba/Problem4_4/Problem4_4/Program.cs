a5ab84b679ffa59c0e6f166a8b05758f8479baac
inabakousuke <34599882+inabakousuke@users.noreply.github.com>
2018-02-21 11:27:46 +0900

    4日目_1

---
+++ basic/inaba/Problem4_4/Problem4_4/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 3;
+            while (i > -1)
+            {
+                Console.WriteLine("num = " + i);
+                i--;
+            }
+        }
+    }
+}
---
+++ basic/inaba/Problem4_6/Problem4_6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 2;
+            while (i < 9)
+            {
+                Console.WriteLine("num = " + i);
+                i += 2;
+            }
+        }
+    }
+}
