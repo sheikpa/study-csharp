db8f0c7cd8abafe134488790cc3c7bbb84057717
inabakousuke <34599882+inabakousuke@users.noreply.github.com>
2018-02-21 11:34:36 +0900

    4日目_3

---
+++ Problem4_11/Problem4_11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rdn = new Random();
+            int min = 100;
+            int max = 1;
+            for (int i = 0; i < 5; i++)
+            {
+                int dice = rdn.Next(1, 101);
+                Console.WriteLine(dice);
+                if (dice < min)
+                {
+                    min = dice;
+                }
+                else if (dice > max)
+                {
+                    max = dice;
+                }
+            }
+            Console.WriteLine("最大値：" + max);
+            Console.WriteLine("最小値：" + min);
+        }
+    }
+}
