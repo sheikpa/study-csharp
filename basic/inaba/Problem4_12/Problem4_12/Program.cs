5f659859f8e6bfe01400f5c26d1c4bfe9c8b6afb
inabakousuke <34599882+inabakousuke@users.noreply.github.com>
2018-02-21 15:18:13 +0900

    4日目_3

---
+++ basic/inaba/Problem4_12/Problem4_12/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem4_12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int guusu = 0;
+            int kisuu = 0;
+            for (int i = 0; i < 5; i++)
+            {
+                int dice = rnd.Next(1, 101);
+                Console.WriteLine(dice);
+                if (dice % 2 == 0)
+                {
+                    guusu++;
+                }
+                else
+                {
+                    kisuu++;
+                }
+            }
+            Console.WriteLine("偶数の数：" + guusu);
+            Console.WriteLine("奇数の数：" + kisuu);
+        }
+    }
+}
